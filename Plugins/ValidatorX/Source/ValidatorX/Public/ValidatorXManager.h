67da76783f92df40e6f1ff807023d2d9e7c5db21
DAlexTools <dalexandrovtools.@gmail.com>
2026-01-06 19:03:03 +0300

    Refactoring Manager class - add comments

--- Plugins/ValidatorX/Source/ValidatorX/Public/ValidatorXManager.h
+++ Plugins/ValidatorX/Source/ValidatorX/Public/ValidatorXManager.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,21 +6,42 @@
 #include "BaseClasses/BlueprintValidatorBase.h"
 
 /**
- * 
+ * @brief Singleton manager for Blueprint validators.
+ *
+ * Manages registration and storage of all `UBlueprintValidatorBase` instances.
+ * Provides global access to registered validators through a singleton pattern.
  */
 class VALIDATORX_API FValidatorXManager
 {
+private:
+	/** @brief Private default constructor for singleton pattern. */
 	FValidatorXManager() {}
+
+	/** @brief Deleted copy constructor to prevent copying. */
 	FValidatorXManager(const FValidatorXManager&) = delete;
+
+	/** @brief Deleted copy assignment operator to prevent copying. */
 	FValidatorXManager& operator=(const FValidatorXManager&) = delete;
 
 public:
+	/**
+	 * @brief Returns the singleton instance of the manager.
+	 *
+	 * @return Reference to the single `FValidatorXManager` instance.
+	 */
 	static FValidatorXManager& Get()
 	{
-		static FValidatorXManager Istance;
-		return Istance;
+		static FValidatorXManager Instance;
+		return Instance;
 	}
 
+	/**
+	 * @brief Registers a validator with the manager.
+	 *
+	 * Adds the validator to the internal list if it is valid.
+	 *
+	 * @param Validator The validator to register. Must not be null.
+	 */
 	void RegisterValidator(UBlueprintValidatorBase* Validator)
 	{
 		if (Validator)
@@ -29,12 +50,17 @@ public:
 		}
 	}
 
+	/**
+	 * @brief Returns all registered validators.
+	 *
+	 * @return A constant reference to the array of registered validators as weak pointers.
+	 */
 	const TArray<TWeakObjectPtr<UBlueprintValidatorBase>>& GetValidators() const
 	{
 		return Validators;
 	}
 
 private:
+	/** @brief Array storing all registered validators as weak object pointers. */
 	TArray<TWeakObjectPtr<UBlueprintValidatorBase>> Validators;
-
 };
