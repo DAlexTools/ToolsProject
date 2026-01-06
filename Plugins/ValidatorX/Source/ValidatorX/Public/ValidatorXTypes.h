89e460362915e3f031781cfe9f823fabd6cae8cd
DAlexTools <dalexandrovtools.@gmail.com>
2026-01-06 18:51:59 +0300

    Add  ValidatorListColumns data

---
+++ Plugins/ValidatorX/Source/ValidatorX/Public/ValidatorXTypes.h
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace ValidatorListColumns
+{
+	static const FName ColumnID_Type("Type");
+	static const FName ColumnID_Name("Name");
+	static const FName ColumnID_Button("Button");
+} // namespace ValidatorListColumns
