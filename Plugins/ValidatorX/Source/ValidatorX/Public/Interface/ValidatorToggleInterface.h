9f41de36f88e996b71a0a55b945745a6b7534660
DAlexTools <dalexandrovtools.@gmail.com>
2026-01-06 18:57:20 +0300

    Refactoring Interface Add Comment

--- Plugins/ValidatorX/Source/ValidatorX/Public/Interface/ValidatorToggleInterface.h
+++ Plugins/ValidatorX/Source/ValidatorX/Public/Interface/ValidatorToggleInterface.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,7 +14,10 @@ class UValidatorToggleInterface : public UInterface
 };
 
 /**
- * 
+ * @brief Interface for validators that can be toggled on or off.
+ *
+ * Classes implementing this interface must provide functionality to enable,
+ * disable, or toggle their validation state.
  */
 class VALIDATORX_API IValidatorToggleInterface
 {
@@ -22,14 +25,17 @@ class VALIDATORX_API IValidatorToggleInterface
 
 public:
 	/**
-	 * Toggles the enabled state of this validator
+	 * @brief Toggles the enabled state of this validator.
+	 *
+	 * If the validator is currently enabled, it will be disabled.
+	 * If it is currently disabled, it will be enabled.
 	 */
 	virtual void ToggleValidationEnabled() = 0;
 
 	/**
-	 * Sets the enabled state of this validator
+	 * @brief Sets the enabled state of this validator.
 	 *
-	 * @param bEnabled  True to enable validation, false to disable
+	 * @param bEnabled True to enable validation, false to disable it.
 	 */
 	virtual void SetValidationEnabled(bool bEnabled) = 0;
 };
