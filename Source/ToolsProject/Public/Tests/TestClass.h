c643d854b16da6bf205e3075a826016dcf0ce5d1
Dmitriy Alexandrov <dalexandrovtools@gmail.com>
2026-07-14 19:05:52 +0300

    Test UNotepad

--- Source/ToolsProject/Public/Tests/TestClass.h
+++ Source/ToolsProject/Public/Tests/TestClass.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -10,6 +10,11 @@
 class TOOLSPROJECT_API TestClass
 {
 public:
+
+	TestClass(int32 a)
+	{
+	}
+	
 	TestClass();
 	~TestClass();
 };
