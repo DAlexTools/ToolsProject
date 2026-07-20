775ce78a450a6394a30f69659249b0e891d893c7
Dmitriy Alexandrov <dalexandrovtools@gmail.com>
2026-07-21 01:32:26 +0300

    added comments in SNotepadTabStrip.h

--- Plugins/UNotepad/Source/UNotepad/Public/UI/SNotepadTabStrip.h
+++ Plugins/UNotepad/Source/UNotepad/Public/UI/SNotepadTabStrip.h
@@ -8,27 +8,77 @@
 
 class SScrollBox;
 
+/** Delegate invoked for document-related actions. */
 DECLARE_DELEGATE_OneParam(FNotepadDocumentAction, TSharedPtr<FNotepadDocument>);
 
+/**
+ * @brief Tab strip widget for managing open documents.
+ *
+ * Displays open documents as tabs and allows selecting or closing them.
+ */
 class UNOTEPAD_API SNotepadTabStrip : public SCompoundWidget
 {
 public:
 	SLATE_BEGIN_ARGS(SNotepadTabStrip) {}
+		
+		/** Called when a document tab is selected. */
 		SLATE_EVENT(FNotepadDocumentAction, OnDocumentSelected)
+		
+		/** Called when a document tab is closed. */
 		SLATE_EVENT(FNotepadDocumentAction, OnDocumentClosed)
 	SLATE_END_ARGS()
 
+	/**
+	 * @brief Constructs the tab strip widget.
+	 *
+	 * @param InArgs Slate construction arguments.
+	 */
 	void Construct(const FArguments& InArgs);
+
+	/**
+	 * @brief Updates the displayed document tabs.
+	 *
+	 * @param InDocuments Collection of open documents.
+	 * @param InActiveDocument Currently active document.
+	 */
 	void SetDocuments(const TArray<TSharedPtr<FNotepadDocument>>& InDocuments, TSharedPtr<FNotepadDocument> InActiveDocument);
 
 private:
+	/**
+	 * @brief Rebuilds the tab strip.
+	 */
 	void Rebuild();
+
+	/**
+	 * @brief Selects the specified document.
+	 *
+	 * @param Document Document to activate.
+	 *
+	 * @return Slate reply indicating whether the event was handled.
+	 */
 	FReply SelectDocument(TSharedPtr<FNotepadDocument> Document);
+
+	/**
+	 * @brief Closes the specified document.
+	 *
+	 * @param Document Document to close.
+	 *
+	 * @return Slate reply indicating whether the event was handled.
+	 */
 	FReply CloseDocument(TSharedPtr<FNotepadDocument> Document);
 
+	/** Collection of open documents. */
 	TArray<TSharedPtr<FNotepadDocument>> Documents;
+
+	/** Currently active document. */
 	TSharedPtr<FNotepadDocument> ActiveDocument;
+
+	/** Scroll box containing the document tabs. */
 	TSharedPtr<SScrollBox> TabStrip;
+
+	/** Delegate fired when a document is selected. */
 	FNotepadDocumentAction OnDocumentSelected;
+
+	/** Delegate fired when a document is closed. */
 	FNotepadDocumentAction OnDocumentClosed;
 };
